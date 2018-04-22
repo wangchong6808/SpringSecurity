baaef31d586c8c291b84e4f63435e4bf8222e2aa
Wang Chong <wangchong@twcns-MacBook-Pro-2.local>
2018-04-22 18:01:53 +0800

    basic role based access control

---
+++ src/main/java/org/springframework/security/samples/config/RoleSecurityConfig.java
@@ -0,0 +1,8 @@
+package org.springframework.security.samples.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class RoleSecurityConfig {
+
+}
